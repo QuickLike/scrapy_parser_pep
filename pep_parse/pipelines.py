41d139152cfcfceb8fd8ec3006f901dc6316030e
Eduard Vlasov <59623754+QuickLike@users.noreply.github.com>
2024-09-04 17:13:27 +0300

    Update pipelines.py

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -9,10 +9,10 @@ from pep_parse.settings import BASE_DIR, DATE_FORMAT, SAVE_PATH
 class PepParsePipeline:
     def open_spider(self, spider):
         self.status_counts = defaultdict(int)
-        os.makedirs(BASE_DIR / SAVE_PATH, exist_ok=True)
 
     def process_item(self, item, spider):
         self.status_counts[item['status']] += 1
+        os.makedirs(BASE_DIR / SAVE_PATH, exist_ok=True)
         return item
 
     def close_spider(self, spider):
