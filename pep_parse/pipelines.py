1117f7c9a7d8fc717a9a342691737ab79cd692d2
QuickLike <QuickLikerfree@gmail.com>
2024-09-04 18:22:49 +0300

    .

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,18 +1,21 @@
 import csv
 import os
-from collections import defaultdict
 from datetime import datetime
+from collections import defaultdict
 
 from pep_parse.settings import BASE_DIR, DATE_FORMAT, SAVE_PATH
 
 
 class PepParsePipeline:
+
+    def __init__(self):
+        os.makedirs(BASE_DIR / SAVE_PATH, exist_ok=True)
+
     def open_spider(self, spider):
         self.status_counts = defaultdict(int)
 
     def process_item(self, item, spider):
         self.status_counts[item['status']] += 1
-        os.makedirs(BASE_DIR / SAVE_PATH, exist_ok=True)
         return item
 
     def close_spider(self, spider):
